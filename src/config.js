991eabe67c25453dda8d66f0e6f84230c908879f
TechAsad1 <techinfo517@gmail.com>
2025-10-05 05:17:06 +0500

    Updates

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
-    url: "http://membership.tryasp.net/api/",
-    urlImage: "http://membership.tryasp.net/",
+    url: "https://membership.tryasp.net/api/",
+    urlImage: "https://membership.tryasp.net/",
     // url: "http://localhost:5247/api/",
     // urlImage: "http://localhost:5247/",
 };
