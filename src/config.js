782f63f5599bffb1680eae80b8697dbe4047e7f8
TechAsad1 <techinfo517@gmail.com>
2025-11-03 14:50:11 +0500

    Url Update

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
-    url: "https://membership.tryasp.net/api/",
-    urlImage: "https://membership.tryasp.net/",
+    url: "https://membership.itmechanix.com/api/",
+    urlImage: "https://membership.itmechanix.com/",
     // url: "http://localhost:5247/api/",
     // urlImage: "http://localhost:5247/",
 };
